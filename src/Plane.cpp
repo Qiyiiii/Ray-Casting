c3fd9ed71b20990632dee09f205e986cdd106e01
QIyi Zhang <qiyi.zhang@mai.utoronto.ca>
2022-12-24 21:22:19 -0500

    ray casting

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -5,8 +5,14 @@ bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
+  n = normal.normalized();
+   if (n.dot(ray.direction) == 0) { // ray parallel to plane
+    return false;
+  }
+  else{
+    t = -n.dot(ray.origin - point) / (n.dot(ray.direction));
+  }
+  return t >= min_t;
   ////////////////////////////////////////////////////////////////////////////
 }
 
